68d8e603f34b6d9a569220f67db036f98d6f109e
vanesuarez <vanesuarez2301@gmail.com>
2023-10-19 20:34:12 -0300

    terms validation

--- js/validacion.js
+++ js/validacion.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const forms = document.querySelectorAll(".needs-validation");
   const password1 = document.getElementById("password1");
   const password2 = document.getElementById("password2");
+  const checkbox = document.getElementById("terminos");
+  const feedbackText = document.getElementById("termsFeedbackText");
 
   forms.forEach(function (form) {
     form.addEventListener("submit", function (event) {
@@ -10,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         event.stopPropagation();
       }
       form.classList.add("was-validated");
+      if (!checkbox.checked) {
+        checkbox.classList.add("is-invalid");
+        feedbackText.classList.add("text-danger");
+      }
     });
   });
 
@@ -20,13 +26,23 @@ document.addEventListener("DOMContentLoaded", function () {
       password2.setCustomValidity('Debe ser igual a "contraseña"');
     }
   }
-  
+
   password1.addEventListener("input", passwordValidation);
   password2.addEventListener("input", passwordValidation);
-  
-  // const checkbox = document.getElementById("terminos");
-  // if (checkbox.checked) {
-  // } else {
-  // }
-  
+
+  function updateFeedbackClasses() {
+    if (checkbox.checked) {
+      checkbox.classList.remove("is-invalid");
+      checkbox.classList.add("is-valid");
+      feedbackText.classList.remove("text-danger");
+    } else {
+      checkbox.classList.remove("is-valid");
+      checkbox.classList.add("is-invalid");
+      feedbackText.classList.add("text-danger");
+    }
+  }
+
+  checkbox.addEventListener("change", updateFeedbackClasses);
+
 });
+
